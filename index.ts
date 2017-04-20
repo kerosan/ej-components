524915da0d8799d8a4569435cd50b4b53c77a310
George <georg@flashdev.od.ua>
2017-04-21 01:44:13 +0300

    Implemented Checkbox

--- index.ts
+++ index.ts
@@ -1,2 +1,4 @@
+export * from './sources/src/components/Button'
+export * from './sources/src/components/Checkbox'
 export * from './sources/src/components/Userpic'
 export * from './sources/src/types/index'
---
+++ sources/src/components/Checkbox.tsx
@@ -0,0 +1,101 @@
+import * as React from 'react';
+
+export interface ICheckboxProps {
+	name?: string;
+	label?: string | JSX.Element;
+	checked?: boolean;
+	className?: string;
+	disabled?: boolean;
+	inline?: boolean;
+	clickCapture?: boolean;
+
+	onChange?: (checked: boolean, name?: string) => void;
+}
+
+export interface ICheckboxState {
+	checked?: boolean;
+}
+
+export class Checkbox extends React.Component<ICheckboxProps, ICheckboxState> {
+
+	constructor(props: ICheckboxProps) {
+		super(props);
+
+		this.state = this.getInitState();
+
+		this.onClick = this.onClick.bind(this);
+	}
+
+	protected getInitState(): ICheckboxState {
+		return {
+			checked: !!this.props.checked,
+		};
+	}
+
+	public componentWillUpdate(nextProps: ICheckboxProps, nextState: ICheckboxState): void {
+		if (this.props.checked !== nextProps.checked) {
+			nextState.checked = nextProps.checked;
+		}
+	}
+
+	protected componentWillReceiveProps(nextProps: ICheckboxProps): void {
+		if (nextProps.checked !== undefined && this.state.checked !== nextProps.checked) {
+			this.setState({
+				...this.state,
+				checked: nextProps.checked,
+			});
+		}
+	}
+
+	public render(): JSX.Element {
+		let spanClassNames: string[] = ['ej-components__checkbox', 'form-group'],
+			labelClassNames: string[] = [],
+			spanElement: JSX.Element;
+
+		if (this.props.className) {
+			spanClassNames.push(this.props.className);
+		}
+
+		if (this.props.inline) {
+			labelClassNames.push('checkbox-inline');
+		}
+
+		if (this.props.disabled) {
+			labelClassNames.push('disabled');
+		}
+
+		if (this.state.checked) {
+			spanElement = <span className='glyphicon glyphicon-ok ej-components__checkbox-icon__checked' />;
+		} else {
+			spanElement = <span className='ej-components__checkbox-icon__unchecked' />;
+		}
+
+		return (
+			<span className={spanClassNames.join(' ')} >
+				<label className={labelClassNames.join(' ')} onClick={this.onClick}>
+					{spanElement}
+					<span>{this.props.label ? this.props.label : this.props.children}</span>
+				</label>
+			</span>
+		);
+	}
+
+	protected onClick(e: React.MouseEvent<HTMLLabelElement>): void {
+		if (this.props.clickCapture) {
+			e.stopPropagation();
+		}
+
+		if (this.props.disabled) {
+			return;
+		}
+
+		this.setState({
+			...this.state,
+			checked: !this.state.checked,
+		});
+
+		if (this.props.onChange) {
+			this.props.onChange(this.state.checked, this.props.name);
+		}
+	}
+}
